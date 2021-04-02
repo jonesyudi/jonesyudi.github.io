d52233ffcde2e115ce2633ec0f911d23838e7cf1
jonesyudi <jonesyudi@gmail.com>
2021-04-01 22:44:01 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-// JavaScript source code
-
-function ShowHelloMessage() {
-    var name = document.getElementById("myname");
-    document.getElementById("hellomessage").innerHTML = "Ola, Mr." + name.value;
-}
-
-document.getElementById("mybutton").onclick = ShowHelloMessage;
